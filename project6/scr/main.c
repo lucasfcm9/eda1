1921027d8e3ef80e621e0358d188ec7f217f1c09
Gabriel Davi <gdavi.gd@gmail.com>
2019-06-26 10:02:35 -0300

    ciodos

--- project6/scr/main.c
+++ project6/scr/main.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 typedef struct _neuron{
   struct _neuron *link;
-  int *w;
-  int bias;
+  double *w;
+  double bias;
   double s;
 }Neuron;
 
@@ -16,12 +16,24 @@ int main(int argc, char *argv[ ]){
   Neuron hidden_layer[hidden_size];
   Neuron final_layer;
 
+  //linking each neuron to each other
   for(int i = 0; i < 10; i++){
     ent_layer[i].link = (Neuron*) malloc(hidden_size*sizeof(Neuron));
+    printf("--------------------------------CAMADA DE ENTRADA: %d\n", &ent_layer[i]);
     for(int j = 0; j < hidden_size; j++){
-      ent_layer[i].link[j] = hidden_layer[j];
-      hidden_layer[j] = final_layer;
+      ent_layer[i].link[j].link = &hidden_layer[j];
+      hidden_layer[j].link = (Neuron*) malloc(sizeof(Neuron));
+      hidden_layer[j].link->link = &final_layer;
+
+      printf("CAMADA DE OCULTA NO LINK: %d\n", ent_layer[i].link[j].link);
+      printf("CAMADA DE OCULTA NORMAL: %d\n", &hidden_layer[j]);
+      printf("CAMADA DE OCULTA FINAL: %d\n", hidden_layer[j].link->link);
+      printf("CAMADA FINAL: %d\n", &final_layer);
     }
   }
+
+  //
+  
+
   return 0;
 }
