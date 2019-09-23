493927fe3448572ee9a04acc1edb6d4d11f58c97
Lucas <lucasfcm9@gmail.com>
2019-09-23 14:33:22 -0300

    Add first project of data structures 1

---
+++ first_project/src/first_program.c
@@ -0,0 +1,56 @@
+/*  Alunos: Lucas Fellipe Carvalho Moreira && Pedro Henrique Queiroz Miranda
+    Matrícula: 16/0133394 && 15/0144474
+    Disciplina: Estruturas de dados 1
+    Professor: Matheus Mendelson
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 10
+#define print(array, length) \
+for(int i = 0; i < length; i++) \
+printf("%.10lf%s", array[i], (i+1 == length ? "\n" : " "));
+#define read(array, length) \
+for(int i = 0; i < length; i++) \
+scanf("%lf", &array[i]);
+typedef double vector;
+#define FOR(x, n) \
+for(int i = 0; i < n; i++)
+
+int fneuronio(vector *input, vector *weight, int n, double threshold)
+{
+    double SOMA_P = 0;
+    FOR(0, n)
+    {
+        SOMA_P += input[i] * weight[i];
+    }
+    printf("Sum: %.10lf\n", SOMA_P);
+    if(SOMA_P > threshold)
+        return 1;
+    return 0;
+}
+
+int main()
+{
+    vector *INPUT, *WEIGHT;
+    INPUT = malloc(sizeof(double) * 10);
+    WEIGHT = malloc(sizeof(double) * 10);
+    printf("Input the 10 values of INPUT:\n");
+    read(INPUT, 10);
+    print(INPUT, 10);
+
+    printf("Input the 10 values of WEIGHT:\n");
+    read(WEIGHT, 10);
+    print(WEIGHT, 10);
+
+    printf("Input the threshold T:\n");
+    double threshold;
+    scanf("%lf", &threshold);
+    if(fneuronio(INPUT, WEIGHT, 10, threshold))
+        printf("Activated neuron!\n");
+    else
+        printf("Inhibited neuron!\n");
+
+    free(INPUT);
+    free(WEIGHT);
+}
